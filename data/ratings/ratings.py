c3849c15a0fe1400e7d399accb3097d4c955fd41
Harshroxnox <harshroxnox@gmail.com>
2023-10-27 17:44:33 +0530

    developed the scripts for creating a final ratings.csv file and converting it to a database named backend_db and then also wrote the scripts for creating a tournament_ratings database required for tournament rankings route

--- data/ratings/ratings.py
+++ data/ratings/ratings.py
@@ -1,14 +1,10 @@
+# Aim: This file basically runs the model against all the final processed data in the data_final.csv
+# file to get the final ratings of all teams in the ratings array and then exports it to a csv
+# file named ratings.csv
+import numpy as np
 import tensorflow as tf
 import pandas as pd
-from pymongo import MongoClient
-import numpy as np
-from datetime import datetime, timedelta
 import csv
-
-# connect to mongodb
-client = MongoClient('mongodb://localhost:27017/')
-db = client['tournament_ratings']
-# collection = db['tournament_ratings']
 
 # Reading the entire data to be processed
 data = pd.read_csv('data_final.csv')
@@ -17,11 +13,8 @@ data = pd.read_csv('data_final.csv')
 model = tf.keras.models.load_model("keras_load_model")
 
 ratings = []
-tournaments = []
 
-first_row = data.head(1)
-tournament_id = first_row["tournamentId"]
-
+# Initialize actions which is necessary for working of model
 ACTIONS = [
     2,
     3,
@@ -36,6 +29,8 @@ ACTIONS = [
 ]
 
 
+# This function takes ratings of A, B, difference and which team won and returns the fluctuations
+# of ratings that must be done.
 def predict(rating_a, rating_b, rating_diff, team_A_wins):
     rating_a = rating_a/800
     rating_b = rating_b/800
@@ -47,8 +42,9 @@ def predict(rating_a, rating_b, rating_diff, team_A_wins):
     return fluctuation
 
 
-def get_rating_and_index(team_id, ratings_arr):
-    for index_teams, teams in enumerate(ratings_arr):
+# Searches for rating and index of a particular team in ratings arr given its id.
+def get_rating_and_index(team_id):
+    for index_teams, teams in enumerate(ratings):
         if teams[0] == team_id:
             return teams[1], index_teams
     # Team not found, add a new entry
@@ -62,59 +58,31 @@ for row in data.itertuples():
     count = count+1
     print(f'count: {count}')
 
-    tournament_id = int(row.tournamentId)
-
-    if tournament_id in tournaments:
-        continue
-    tournaments.append(tournament_id)
-
-    # We will only look at matches that were held between start_date and end_date. The gap between
-    # them is of six months.
-    end_date = int(row.startDate)
-    input_date = datetime.strptime(str(row.startDate), "%Y%m%d")
-    # start date which is date of six months ago
-    start_date = input_date - timedelta(days=30 * 6)
-    start_date = int(start_date.strftime("%Y%m%d"))
-
-    inner_count = 0
-    for inner_row in data.itertuples():
-        inner_count = inner_count+1
-        print(f'inner_count: {inner_count}')
-
-        curr_date = int(inner_row.startDate)
-
-        if (curr_date >= start_date) and (curr_date < end_date):
-
-            ratings = []
-            rating_a, index_a = get_rating_and_index(inner_row.teamA, ratings)
-            rating_b, index_b = get_rating_and_index(inner_row.teamB, ratings)
-
-            if rating_a > rating_b:
-                rating_diff = rating_a - rating_b
-            else:
-                rating_diff = rating_b - rating_a
-
-            fluctuation = predict(rating_a, rating_b, rating_diff, inner_row.result)
-
-            if inner_row.result == 1:
-                ratings[index_a][1] += fluctuation
-                ratings[index_b][1] -= fluctuation
-            else:
-                ratings[index_a][1] -= fluctuation
-                ratings[index_b][1] += fluctuation
-
-    collection = db[str(tournament_id)]
-    for team in ratings:
-        document = {
-            "team_id": str(team[0]),
-            "rating": team[1]
-        }
-        collection.insert_one(document)
     print(ratings)
+    print(" ")
+    # Extract the parameters for current row
+    rating_a, index_a = get_rating_and_index(row.teamA)
+    rating_b, index_b = get_rating_and_index(row.teamB)
 
-"""
-# Insert the final ratings into a ratings.csv file
+    if rating_a > rating_b:
+        rating_diff = rating_a - rating_b
+    else:
+        rating_diff = rating_b - rating_a
+
+    # Get the fluctuation of the model
+    fluctuation = predict(rating_a, rating_b, rating_diff, row.result)
+
+    # Update the ratings in the ratings arr
+    if row.result == 1:
+        ratings[index_a][1] += fluctuation
+        ratings[index_b][1] -= fluctuation
+    else:
+        ratings[index_a][1] -= fluctuation
+        ratings[index_b][1] += fluctuation
+
+# Extract all the ratings of teams from the ratings arr into the ratings.csv file
+
 with open("ratings.csv", 'w', newline='') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerows(ratings)
-"""
+
---
+++ data/ratings/tournament_ratings.py
@@ -0,0 +1,132 @@
+# Aim: This file iterates through the data in the data_final.csv file and for every single unique
+# tournament found in that file it takes into account the data of previous six months from the
+# starting of the tournament and returns the ranking of teams based on the previous six months
+# data.
+# It stores all the rankings data for each tournament in a mongodb database(mongodb instance must
+# be running) the db name is tournament_ratings and in there collections will be named after the
+# tournament Id for which they will contain the rankings of the teams.
+import tensorflow as tf
+import pandas as pd
+from pymongo import MongoClient
+import numpy as np
+from datetime import datetime, timedelta
+
+# connect to mongodb. The name of database is going to be tournament_ratings which will be
+# automatically created on running this script provided the mongod service is running on linux
+# server
+client = MongoClient('mongodb://localhost:27017/')
+db = client['tournament_ratings']
+
+# Reading the entire data to be processed
+data = pd.read_csv('data_final.csv')
+
+# Load the SavedModel
+model = tf.keras.models.load_model("keras_load_model")
+
+# Initialize ratings, tournaments(for keeping track of teams ratings and the tournaments we have
+# already covered)
+ratings = []
+tournaments = []
+
+# Gets tournament Id of first row
+first_row = data.head(1)
+tournament_id = first_row["tournamentId"]
+
+# Initialize actions which is necessary for working of model
+ACTIONS = [
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+]
+
+
+# This function takes ratings of A, B, difference and which team won and returns the fluctuations
+# of ratings that must be done.
+def predict(rating_a, rating_b, rating_diff, team_A_wins):
+    rating_a = rating_a/800
+    rating_b = rating_b/800
+    rating_diff = rating_diff/800
+    state = [rating_a, rating_b, rating_diff, team_A_wins]
+    q_values = model.predict(np.array([state]))
+    action = np.argmax(q_values)
+    fluctuation = ((rating_diff * 800) / ACTIONS[action]) + 20
+    return fluctuation
+
+
+# Searches for rating and index of a particular team in ratings arr given its id.
+def get_rating_and_index(team_id):
+    for index_teams, teams in enumerate(ratings):
+        if teams[0] == team_id:
+            return teams[1], index_teams
+    # Team not found, add a new entry
+    new_team = [team_id, 400]
+    ratings.append(new_team)
+    return 400, len(ratings) - 1
+
+
+count = 0
+for row in data.itertuples():
+    count = count+1
+    print(f'count: {count}')
+
+    tournament_id = int(row.tournamentId)
+
+    # If tournament already covered skip
+    if tournament_id in tournaments:
+        continue
+    tournaments.append(tournament_id)
+
+    # We will only look at matches that were held between start_date and end_date. The gap between
+    # them is of six months.
+    end_date = int(row.startDate)
+    input_date = datetime.strptime(str(row.startDate), "%Y%m%d")
+    # start date which is date of six months ago
+    start_date = input_date - timedelta(days=30 * 6)
+    start_date = int(start_date.strftime("%Y%m%d"))
+    # Reinitialize ratings arr
+    ratings = []
+    inner_count = 0
+    for inner_row in data.itertuples():
+        inner_count = inner_count+1
+        print(f'inner_count: {inner_count}')
+
+        curr_date = int(inner_row.startDate)
+        # Only consider that row if curr_date lies between start and end date.
+        if (curr_date >= start_date) and (curr_date < end_date):
+            # Extract all the parameters
+            rating_a, index_a = get_rating_and_index(inner_row.teamA)
+            rating_b, index_b = get_rating_and_index(inner_row.teamB)
+
+            if rating_a > rating_b:
+                rating_diff = rating_a - rating_b
+            else:
+                rating_diff = rating_b - rating_a
+
+            # Get the fluctuation of the model
+            fluctuation = predict(rating_a, rating_b, rating_diff, inner_row.result)
+
+            # Update the ratings in the ratings arr
+            if inner_row.result == 1:
+                ratings[index_a][1] += fluctuation
+                ratings[index_b][1] -= fluctuation
+            else:
+                ratings[index_a][1] -= fluctuation
+                ratings[index_b][1] += fluctuation
+
+    # Finally insert all ratings of teams in ratings arr in a mongodb collection
+    collection = db[str(tournament_id)]
+    for team in ratings:
+        document = {
+            "team_id": str(team[0]),
+            "rating": team[1]
+        }
+        collection.insert_one(document)
+    print(ratings)
+
